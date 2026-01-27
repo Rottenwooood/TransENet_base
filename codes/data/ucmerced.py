1e5c16beed5f0180bba766384fca0e4186e0b1b7
Rottenwooood <2535771636@qq.com>
2026-01-27 12:07:17 +0800

    srdata.py与ucmerced.py

--- codes/data/ucmerced.py
+++ codes/data/ucmerced.py
@@ -1,5 +1,3 @@
-
-
 import os
 import glob
 from data import srdata
@@ -13,11 +11,16 @@ class UCMercedDataset(srdata.SRData):
         self.scale = args.scale[0]
 
     def _set_filesystem(self):
-        self.dir_hr = os.path.join(self.root_dir, 'HR')
-        self.dir_lr = os.path.join(self.root_dir, 'LR_x' + str(self.scale))
-        self.ext = '.tif'
+        # For .pt files, we don't need to set up directory paths
+        if self.args.ext != 'pt':
+            self.dir_hr = os.path.join(self.root_dir, 'HR_x4')
+            self.dir_lr = os.path.join(self.root_dir, 'LR_x4' )
+            self.ext = '.tif'
 
     def _scan(self):
+        # For .pt files, we don't need to scan directories
+        if self.args.ext == 'pt':
+            return [], []
         list_hr = glob.glob(os.path.join(self.dir_hr, '*'+self.ext))
         list_lr = []
         for i in range(len(list_hr)):
@@ -25,20 +28,6 @@ class UCMercedDataset(srdata.SRData):
             list_lr.append(os.path.join(self.dir_lr, filename))
 
         return list_hr, list_lr
-
     def __len__(self):
         return len(self.hr_img_dirs)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
