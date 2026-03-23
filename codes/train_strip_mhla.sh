c9e292736665331121db6f92a6a752e40ef6fc1d
Rottenwooood <2535771636@qq.com>
2026-03-23 23:53:12 +0800

    +v2

--- codes/train_strip_mhla.sh
+++ codes/train_strip_mhla.sh
@@ -5,7 +5,8 @@ export http_proxy="http://127.0.0.1:7890"
 export https_proxy="http://127.0.0.1:7890"
 
 # Training
-# python user/batch_train.py --config user/experiments_config5_02_s1_trans_strip_mhla.json
+python user/batch_train.py --config user/experiments_config5_02_s1_trans_strip_mhla.json
+python user/batch_train.py --config user/experiments_config5_02_s1_trans_strip_mhla.json
 
 # Inference and Evaluation
 python demo_deploy.py --model symunet_pretrain_strip_mhla \
@@ -14,8 +15,8 @@ python demo_deploy.py --model symunet_pretrain_strip_mhla \
     --symunet_pretrain_dec_blk_nums 6,4 \
     --dataset UCMerced \
     --scale 4 \
-    --pre_train ../experiment/s1_trans_strip_mhla_001_s1_trans_strip_mhla_w32/model/model_best.pt \
-    --dir_out ../experiment/results/s1_trans_strip_mhla_46W32/x4
+    --pre_train ../experiment/s1_trans_strip_mhla_v2_001_s1_trans_strip_mhla_v2_w32/model/model_best.pt \
+    --dir_out ../experiment/results/s1_trans_strip_mhla_v2_46W32/x4
 
 # Calculate PSNR/SSIM
-python calculate_PSNR_SSIM.py --folder_Gen ../experiment/results/s1_trans_strip_mhla_46W32/x4 | tail -n 1 >> results.txt
+python calculate_PSNR_SSIM.py --folder_Gen ../experiment/results/s1_trans_strip_mhla_v2_46W32/x4 | tail -n 1 >> results.txt
