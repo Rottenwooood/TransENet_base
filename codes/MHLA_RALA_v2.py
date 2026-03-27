592f250be4409fee4bd09120f8e7e7b94d4fa38e
Rottenwooood <2535771636@qq.com>
2026-03-27 22:28:48 +0800

    添加v2，添加缓存，减小无意义计算量，去除SiLU，防止过拟合

--- codes/MHLA_RALA_v2.py
+++ codes/MHLA_RALA_v2.py
@@ -4,6 +4,7 @@ from einops import rearrange
 import torch.nn.functional as F
 import math
 
+
 class DynamicBlockDistanceWeight(nn.Module):
     """
     动态生成块间距离权重，支持任意分辨率带来的不同 Block 数量。
@@ -57,6 +58,7 @@ class DynamicBlockDistanceWeight(nn.Module):
 
         # return dist_matrix # Fallback
 
+
 class MHLA_Normed_Torch_Dynamic_RALA(nn.Module):
     def __init__(self, dim, heads=4, dim_head=None, dropout=0.1, qk_norm=False, transform="cos", window_size=49):
         super().__init__()
@@ -81,8 +83,8 @@ class MHLA_Normed_Torch_Dynamic_RALA(nn.Module):
         # RALA 门控：用于高频恢复
         # ==========================================
         self.rala_gate = nn.Sequential(
-            nn.Linear(dim, inner_dim),  # inner_dim = head_dim * heads
-            nn.SiLU()                    # SiLU 曲线平滑利于超分
+            nn.Linear(dim, inner_dim,bias=False),  # inner_dim = head_dim * heads
+            # nn.SiLU()                    # SiLU 曲线平滑利于超分
         )
 
     def _mlp_lepe(self, x, pieces_h, pieces_w):
